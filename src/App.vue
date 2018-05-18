5f0f0de79e0f8f1fdcaa3ecaa3a6167dd9048a64
Heyo <csxc.me@gmail.com>
2018-05-18 20:43:25 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <MHeader/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header'
+export default {
+  components: {
+    MHeader
+  }
+}
+</script>
+
+<style lang="stylus">
+@import './common/stylus/variable.styl'
+
+#app
+  color: $color-theme
+</style>
