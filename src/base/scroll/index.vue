34b60093b40574fe8624a1df3749aa38bb9602ae
Heyo <csxc.me@gmail.com>
2018-06-07 17:37:40 +0800

    下拉搜索框

--- src/base/scroll/index.vue
+++ src/base/scroll/index.vue
@@ -22,7 +22,11 @@ export default {
     listenScroll: {
       type: Boolean,
       default: false
-    }
+    },
+    pullup: {
+      type: Boolean,
+      default: false
+    },
   },
   mounted() {
     setTimeout(() => {
@@ -41,6 +45,13 @@ export default {
       if (this.listenScroll) {
         this.scroll.on('scroll', (pos) => {
           this.$emit('scroll', pos)
+        })
+      }
+      if (this.pullup) {
+        this.scroll.on('scrollEnd', () => {
+          if (this.scroll.y <= (this.scroll.maxScrollY + 50)) {
+            this.$emit('scrollToEnd')
+          }
         })
       }
     },
